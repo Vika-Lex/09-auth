893381ecc77cbebbf5427a462d85458f09ddfb48
Viktoriia Kolesnik <vika.notariat@ukr.net>
2025-08-29 17:38:10 +0300

    ref HW 09-auth

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,37 +1,55 @@
 import {api} from "@/app/api/api";
+import { cookies } from "next/headers";
 import {NextResponse} from "next/server";
 
-interface RouteParams {
-    params: {
-        id: string;
-    };
-}
+type Props = {
+    params: Promise<{ id: string }>;
+};
 
-export async function GET(request: Request,  { params }: RouteParams) {
- const { id } = params;
-    try {
-        const res = await api.get(`/notes/${id}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-            }
-        })
-        return NextResponse.json(res.data);
+export async function GET(request: Request,  { params }: Props) {
+     try {
+         const cookieStore = await cookies();
+         const { id } = await params;
+         const res = await api(`/notes/${id}`, {
+             headers: {
+                 Cookie: cookieStore.toString(),
+             },
+         });
+         return NextResponse.json(res.data, { status: res.status });
     } catch (e) {
         return NextResponse.json({error: 'Failed to get by id'}, {status: 500});
     }
 }
 
-export async function DELETE(request: Request, { params }: RouteParams) {
+export async function DELETE(request: Request, { params }: Props) {
     try {
-        const res = await api.delete(`/notes/${params.id}`, {
+        const cookieStore = await cookies();
+        const { id } = await params;
+
+        const res = await api.delete(`/notes/${id}`, {
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`
-            }
-        })
-        return NextResponse.json(res.data);
+                Cookie: cookieStore.toString(),
+            },
+        });
+        return NextResponse.json(res.data, { status: res.status });
     } catch (e) {
         return NextResponse.json({error: 'Failed to delete'}, {status: 500});
+    }
+}
+
+export async function PATCH(request: Request, { params }: Props) {
+    try {
+        const cookieStore = await cookies();
+        const { id } = await params;
+        const body = await request.json();
+
+        const res = await api.patch(`/notes/${id}`, body, {
+            headers: {
+                Cookie: cookieStore.toString(),
+            },
+        });
+        return NextResponse.json(res.data, { status: res.status });
+    } catch (error) {
+        return NextResponse.json({error: 'Failed to change'}, {status: 500});
     }
 }
